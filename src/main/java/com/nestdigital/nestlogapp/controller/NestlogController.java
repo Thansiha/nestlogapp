2fc202b6ddc79918d3ba67ae503f0ab36caa8d79
Ayisha Thansiha <ayishathansi234@gmail.com>
2022-09-12 12:19:15 +0530

    add search employee

--- src/main/java/com/nestdigital/nestlogapp/controller/NestlogController.java
+++ src/main/java/com/nestdigital/nestlogapp/controller/NestlogController.java
@@ -1,15 +1,29 @@
 package com.nestdigital.nestlogapp.controller;
 
+import com.nestdigital.nestlogapp.model.NestModel;
 import com.nestdigital.nestlogapp.model.dao.NestlogDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class NestlogController {
     @Autowired
     NestlogDao dao;
+    @CrossOrigin(origins = "*")
+    @PostMapping("/addemployee")
+    public String addemp (@RequestBody NestModel addemployee){
+        dao.save(addemployee);
+        return "employees added successfully";}
+
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/searchemp")
+    public List<NestModel>search(){
+        return (List<NestModel>)
+                dao.findById(id);
+    }
 
 
 
